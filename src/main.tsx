89785622c54d6edc540eea3beb46f65040ed6b25
xtimxlin <xtimxlin@gmail.com>
2024-12-19 13:34:01 -0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,9 @@ import  { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 // import { BrowserRouter, Routers, Route } from "react-router-dom"
-import { BrowserRouter, Routes, Route  } from 'react-router-dom';
-
-import App from './App.tsx'
+// import { BrowserRouter, Routes, Route  } from 'react-router-dom';
+//
+// import App from './App.tsx'
 // import NotesPage from "./notes/NotesPage.tsx";
 
 createRoot(document.getElementById('root')!).render(
