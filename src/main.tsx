bb4b60a95d3c68c5cf9cf247b3db977a8b28fa67
xtimxlin <xtimxlin@gmail.com>
2024-12-19 14:00:33 -0500

    test again

--- src/main.tsx
+++ src/main.tsx
@@ -1,23 +1,10 @@
 import  { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-// import { BrowserRouter, Routers, Route } from "react-router-dom"
-// import { BrowserRouter, Routes, Route  } from 'react-router-dom';
-//
 import App from './App.tsx'
-// import NotesPage from "./notes/NotesPage.tsx";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-      <h1>hello</h1>
-      {/*<BrowserRouter basename="/Notes/">*/}
-      {/*    <Routes>*/}
-      {/*        <Route path="*" element={<App/>} />*/}
-      {/*    </Routes>*/}
-
-      {/*</BrowserRouter>*/}
-
       <App />
-      {/*<NotesPage />*/}
   </StrictMode>,
 )
