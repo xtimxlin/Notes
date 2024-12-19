1d3346a84926924f53dcffbb440933f0ad096a8c
xtimxlin <xtimxlin@gmail.com>
2024-12-19 11:55:27 -0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,21 @@
 import  { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { BrowserRouter, Routers, Route } from "react-router-dom"
+// import { BrowserRouter, Routers, Route } from "react-router-dom"
+import { BrowserRouter, Routes, Route  } from 'react-router-dom';
 
 import App from './App.tsx'
 // import NotesPage from "./notes/NotesPage.tsx";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-      <BrowserRouter>
-          <Routers>
+      <BrowserRouter basename="/Notes/">
+          <Routes>
               <Route path="*" element={<App/>} />
-          </Routers>
+          </Routes>
 
-      </BrowserRouter>
+      </BrowserRouter>pm start
+
     {/*<App />*/}
       {/*<NotesPage />*/}
   </StrictMode>,
